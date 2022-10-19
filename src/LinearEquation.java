3082b8edf7ec44313e6ccd011c806305edbb0e99
ImamM5 <imamm@nycstudents.net>
2022-10-19 09:12:57 -0400

    new code

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation
+{
+    private double x1;
+    private double y1;
+    private double x2;
+    private double y2;
+
+    public LinearEquation (int x1, int y1, int x2, int y2)
+    {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x1;
+        this.y2 = x1;
+    }
+
+    public double getDistance()
+    {
+        double distance = Math.sqrt((Math.pow((x2-x1), 2)) + (Math.pow((y2-y1),2)));
+        return Math.round(distance * 100.0)/100.0;
+    }
+
+    public String getEquation()
+    {
+
+    }
+
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class LinearEquationRunner {
+import java.util.Scanner;
+
+public class LinearEquationRunner
+{
+    public static void main(String [] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("------------------------- \nWelcome!");
+        System.out.print("Please enter your coordinate 1 (integers): ");
+        String c1 = input.nextLine();
+        System.out.print("Please enter your coordinate 2 (integer): ");
+        String c2 = input.nextLine();
+
+        int comma1 = c1.indexOf(",");
+        String x1String = c1.substring(1, comma1); //takes x1  out as string
+        String y1String = c1.substring(comma1+2, (c1.length()-1)); //takes y1 out as string
+        int x1 = Integer.parseInt(x1String); //converts x1 string to int
+        int y1 = Integer.parseInt(y1String); // converts y1 string to int
+
+        int comma2 = c2.indexOf(",");
+        String x2String = c2.substring(1, comma2); // takes x2 out as string
+        String y2String = c2.substring(comma2+2, (c2.length()-1)); // takes y2 out as string
+        int x2 = Integer.parseInt(x2String); // converts x2 string to int
+        int y2 = Integer.parseInt(y2String); // converts y2 string to int
+
+
+
+
+
+
+
+
+
+    }
+
 }
