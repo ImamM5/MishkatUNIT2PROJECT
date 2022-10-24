e58a757f4fed8985d73db55c46abd9107cfee15f
ImamM5 <imamm@nycstudents.net>
2022-10-24 08:32:58 -0400

    new code

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -3,6 +3,8 @@ public class LinearEquation {
     private double y1;
     private double x2;
     private double y2;
+    private int num;
+    private int denom;
     private double division;
 
     public LinearEquation(int x1, int y1, int x2, int y2) { // constructor
@@ -12,39 +14,48 @@ public class LinearEquation {
         this.y2 = y2;
     }
 
-    public String getDistance() { // gets the distance using the distance formula
+    public double getDistance() { // gets the distance using the distance formula
         double distance = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
-        return String.format("%.2f", distance);
+        return toRound(distance);
     }
 
-    public String getSlope() {
-        int num = (int) y2 - (int) y1;
-        int denom = (int) x2 - (int) x1;
+    public double getSlope() {
+        this.num = (int) y2 - (int) y1;
+        this.denom = (int) x2 - (int) x1;
         this.division = (double) num / denom;
-        if ((num % denom) == 0) {
-            return Integer.toString((int) division); // returns the value if num/denom is a whole number
-        } else if ((num < 0) || (denom < 0)) {
-            return "-" + Math.abs(num) + "/" + Math.abs(denom); // adds "-" to the beginning if either num or denom are negative
-        } else {
-            return num + "/" + denom; // else if they're neither negative or a whole number this just returns the value as a fraction
-        }
-}
-        public String getYIntercept()
+        return division;
+    }
+
+        public double getYIntercept()
         {
             double slopePoint = division*(x1);
             double yIntercept = (y1 - slopePoint);
-            return String.format("%.2f", yIntercept);
+            return toRound(yIntercept);
+        }
+
+        public String getSlopeForEquation()
+        {
+            this.num = (int) y2 - (int) y1;
+            this.denom = (int) x2 - (int) x1;
+            if ((num % denom) == 0) {
+                return Integer.toString((int)getSlope()); // returns the value if num/denom is a whole number
+            }
+            else if ((num < 0) || (denom < 0)) {
+                return "-" + Math.abs(num) + "/" + Math.abs(denom); // adds "-" to the beginning if either num or denom are negative
+            }
+            else {
+                return num + "/" + denom; // else if they're neither negative or a whole number this just returns the value as a fraction
+            }
         }
 
         public String getEquation()
         {
-            double yIntercept= Double.parseDouble(getYIntercept());
-            if ((yIntercept > 0) && (y1 != y2))
+            if ((getYIntercept() > 0) && (y1 != y2))
             {
-                return "y = "+ getSlope() + "x + "+ getYIntercept(); // if y intercept is positive it returns the equation with a "+"
-            } else if ((yIntercept < 0) && (y1 != y2))
+                return "y = "+ getSlopeForEquation() + "x + "+ Math.abs(getYIntercept()); // if y intercept is positive it returns the equation with a "+"
+            } else if ((getYIntercept() < 0) && (y1 != y2))
             {
-                return "y = "+ getSlope() + "x - "+ Math.abs(yIntercept); // if y intercept is negative it returns the equation with a "-"
+                return "y = "+ getSlopeForEquation() + "x - "+ Math.abs(getYIntercept()); // if y intercept is negative it returns the equation with a "-"
             } else {
                 return "y = "+ getYIntercept();
             }
@@ -67,9 +78,14 @@ public class LinearEquation {
         {
             double num = y2 - y1;
             double denom = x2 - x1;
-            double newY= (num/denom)* newX + Double.parseDouble(getYIntercept());
+            double newY= (num/denom)* newX + getYIntercept();
             return "("+ newX+ ", "+ newY + ")";
         }
+
+        public double toRound(double x) //helper method to round the decimals
+    {
+        return Math.round(x * 100.0)/ 100.0;
+    }
 
 
 
